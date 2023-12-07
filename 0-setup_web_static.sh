2eaa72f09c0f0a0b0caff3528c8196fac204dfcb
Henry Arnold <devharnold@gmail.com>
2023-12-07 19:52:12 +0300

    SetupWebStatic

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+#Install Nginx if not currently installed
+#And sets up a web server for deployment of web_static
+
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install -y nginx
+fi 
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared
+echo "Holberton School" > /data/web_static/releases/test/index.html
+
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+sudo chgrp -R ubuntu:ubuntu /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root /var/www/html;
+    index index.html index.htm;
+
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://sarova.com
+    }
+    error_page 404 /404.html;
+    location /404 {
+        root /var/www/html;
+        internal;
+    }
+}" > /etc/nginx/sites-available/default
+service nginx restart

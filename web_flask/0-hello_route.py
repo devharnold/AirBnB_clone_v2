b1f2b3f227eda4cc5a4ad6e06dfce127a4aab8b9
Henry Arnold <devharnold@gmail.com>
2024-01-25 22:26:50 +0300

    Do some work

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,12 +2,12 @@
 """Start a Flask application that 
     listens on 0.0.0.0 port=5000
     Routes: 
-        /airbnb-onepage/: To display 'Hello HBNB!'"""
+        /: To display 'Hello HBNB!'"""
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/airbnb-onepage/", strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def display_hello_hbnb(text):
     """Display hello hbnb """
     return "Hello, HBNB!"
